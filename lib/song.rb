635d89250a1a77365b4260109118ae380a3bc71f
Joseph Romero <joeromero47@gmail.com>
2020-05-14 03:27:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ class Song
   
   def initialize(name)
     @name = name 
-    @@all << self
+    @artist = nil
   end
   
   def self.all 
@@ -16,6 +16,8 @@ class Song
     @@all << self 
     self 
   end
+  
+  def artist=(artist)
   def self.new_by_filename(filename)
     artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
     song.artist = Artist.find_or_create_by_name(artist_name)
