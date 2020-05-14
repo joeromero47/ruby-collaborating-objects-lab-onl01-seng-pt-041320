463bf756e4ff9db358ac1be497b0eaf189014e0a
Joseph Romero <joeromero47@gmail.com>
2020-05-14 03:27:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,10 @@ class Song
     @@all
   end
   
+  def save 
+    @@all << self 
+    self 
+  end
   def self.new_by_filename(filename)
     artist_name, song_name, genre_name = filename.chomp(".mp3").split(" - ")
     song.artist = Artist.find_or_create_by_name(artist_name)
